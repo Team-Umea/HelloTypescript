3d92368d4f9650d50a9ec5f97f2b4cb0a1c5c017
FrankNorq <168375928+FrankNorq@users.noreply.github.com>
2024-12-06 10:44:57 +0100

    Type done, Button done

--- index.ts
+++ index.ts
@@ -1,1 +1,108 @@
-console.log("Hello world"); 
+const mainBox = document.querySelector(".main");
+const pizzaButton = document.getElementById("pizzaButton");
+const saladButton = document.getElementById("saladButton");
+const drinkButton = document.getElementById("drinkButton");
+type Pizza = {
+    id: number,
+    name:string,
+    type:string,
+    description:string,
+    toppings:string[],
+    price:number,
+    imgUrl:string,
+}
+type Salad = {
+    id:number,
+    name:string,
+    type:string,
+    description:string,
+    ingredients:string[],
+    price: number,
+    imgUrl:string,
+}
+type Drink = {
+    id:number,
+    name:string,
+    type:string,
+    description:string,
+    price:number,
+    imgUrl:string,
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fetchPizzaMenu = async () => {
+//     const url = 'https://6ldruff9ul.execute-api.eu-north-1.amazonaws.com/menu?type=pizza';
+    
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'accept': 'application/json',
+//             'x-zocom': 'sp-7BTxHCyHhzIME5TI'
+//         }
+//     };
+// }
+
+// Salad
+// curl -X 'GET' \
+//   'https://6ldruff9ul.execute-api.eu-north-1.amazonaws.com/menu?type=salad' \
+//   -H 'accept: application/json' \
+//   -H 'x-zocom: "sp-7BTxHCyHhzIME5TI"'
+// Request URL
+// https://6ldruff9ul.execute-api.eu-north-1.amazonaws.com/menu?type=salad
+
+
+
+
+// Drinks 
+// curl -X 'GET' \
+//   'https://6ldruff9ul.execute-api.eu-north-1.amazonaws.com/menu?type=drink' \
+//   -H 'accept: application/json' \
+//   -H 'x-zocom: "sp-7BTxHCyHhzIME5TI"'
+// Request URL
+// https://6ldruff9ul.execute-api.eu-north-1.amazonaws.com/menu?type=drink
